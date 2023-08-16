c2716e790426527c317a4a04ef99ce96ae1f4c8b
Mikhail Dombrovsky <madombrovsky@gmail.com>
2023-08-16 17:19:04 +0300

    fix result css

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ const login_page = document.getElementById("login");
 
 
 const player1 = form.nickname1;
-const player2 = form.nickname2;
+// const player2 = form.nickname2;
 
 let player1_value = player1.value;
-let player2_value = player2.value;
+// let player2_value = player2.value;
 
 
 
 function clickPlay(event) {
   event.preventDefault();
   player1_value = player1.value;
-  player2_value = player2.value;
+  // player2_value = player2.value;
 
   if (
     !(
-      player1_value.match(/^[A-Za-z0-9]+$/) &&
-      player2_value.match(/^[A-Za-z0-9]+$/)
+      player1_value.match(/^[A-Za-z0-9]+$/)
     )
   ) {
     alert("Please reenter nickname");
@@ -31,114 +30,140 @@ function clickPlay(event) {
 }
 
 //main_page
-
+const main_page = document.querySelector("main");
 const divPhrase = document.querySelector(".phrase");
-
+const result_page = document.querySelector("section.result");
 const imageTarget = document.querySelector(".imageTarget");
 
 const imagesObjects = [
   {
     id_img: 0,
+    points:0,
     link_img: "./meme_for_hackaton/10GUY.jpg",
   },
   {
     id_img: 1,
+    points:0,
     link_img: "./meme_for_hackaton/3059_135068129530.jpg",
   },
   {
     id_img: 2,
+    points:0,
     link_img: "./meme_for_hackaton/Aliens.jpg",
   },
   {
     id_img: 3,
+    points:0,
     link_img: "./meme_for_hackaton/And_Its_Gone.png",
   },
   {
     id_img: 4,
+    points:0,
     link_img: "./meme_for_hackaton/BadLuckBryan.jpg",
   },
   {
     id_img: 5,
+    points:0,
     link_img: "./meme_for_hackaton/c09.jpg",
   },
   {
     id_img: 6,
+    points:0,
     link_img: "./meme_for_hackaton/chubbybubbles.jpg",
   },
   {
     id_img: 7,
+    points:0,
     link_img: "./meme_for_hackaton/confusedtravolta.jpg",
   },
   {
     id_img: 8,
+    points:0,
     link_img: "./meme_for_hackaton/db1.jpg",
   },
   {
     id_img: 9,
+    points:0,
     link_img: "./meme_for_hackaton/drilpic.jpg",
   },
   {
     id_img: 10,
+    points:0,
     link_img: "./meme_for_hackaton/dweller.jpg",
   },
   {
     id_img: 11,
+    points:0,
     link_img: "./meme_for_hackaton/FaZd-hmWYAU3O6d.jpg",
   },
   {
     id_img: 12,
+    points:0,
     link_img: "./meme_for_hackaton/fellowkids.jpg",
   },
   {
     id_img: 13,
+    points:0,
     link_img: "./meme_for_hackaton/FKBoXcKVkAIJ7Ia.jpg",
   },
   {
     id_img: 14,
+    points:0,
     link_img: "./meme_for_hackaton/FmtBz6JXoA8hTX8.jpg",
   },
   {
     id_img: 15,
+    points:0,
     link_img: "./meme_for_hackaton/hidethepainharold.jpg",
   },
   {
     id_img: 16,
+    points:0,
     link_img: "./meme_for_hackaton/jordan.jpg",
   },
   {
     id_img: 17,
+    points:0,
     link_img: "./meme_for_hackaton/nedstark.jpg",
   },
   {
     id_img: 18,
+    points:0,
     link_img: "./meme_for_hackaton/rick.jpg",
   },
   {
     id_img: 19,
+    points:0,
     link_img: "./meme_for_hackaton/SadKeanu.jpg",
   },
   {
     id_img: 20,
+    points:0,
     link_img: "./meme_for_hackaton/sideeyechloe.jpg",
   },
   {
     id_img: 21,
+    points:0,
     link_img: "./meme_for_hackaton/thumb.png",
   },
   {
     id_img: 22,
+    points:0,
     link_img: "./meme_for_hackaton/Untitled-1.png",
   },
   {
     id_img: 23,
+    points:0,
     link_img: "./meme_for_hackaton/v4ic7qanr7i.jpg",
   },
   {
     id_img: 24,
+    points:0,
     link_img: "./meme_for_hackaton/WAT.jpg",
   },
   {
     id_img: 25,
+    points:0,
     link_img: "./meme_for_hackaton/x.jpg",
   },
 ];
@@ -146,62 +171,58 @@ const imagesObjects = [
 const questionsObjects = [
   {
     id_answer: 0,
-    questionsImg: [1],
     question: "When received my first salary in an Hi-Tech company",
   },
   {
     id_answer: 1,
-    questionsImg: [2],
     question: "When received the first task in the Hi-Tech company",
   },
   {
     id_answer: 2,
-    questionsImg: [3],
-    question: "When received the first task in the Hi-Tech company",
+    question: "When you failed your first interview",
   },
   {
     id_answer: 3,
-    questionsImg: [4],
     question: "when you look at classmates who do not work in IT",
   },
   {
     id_answer: 4,
-    questionsImg: [5],
-    question: "when to do a hackathon before thursday",
+    question: "when you must have done a hackathon before thursday",
   },
   {
     id_answer: 5,
-    questionsImg: [6],
-    question: "when to refactor",
+    question: "when you have to refactor",
   },
   {
     id_answer: 6,
-    questionsImg: [7],
     question: "when found the answer on Stack Overflow",
   },
   {
     id_answer: 7,
-    questionsImg: [8],
     question: "got a job in a startup",
   },
   {
     id_answer: 8,
-    questionsImg: [9],
     question: "reaction to the first question in a technical interview",
   },
   {
     id_answer: 9,
-    questionsImg: [10],
-    question: "when the senior developer looks at you before giving a task",
+    question: "how the senior developer looks at you before giving a task",
   },
 ];
 
-let cards = document.querySelector(".cards");
+fillPoints()
 
+function fillPoints(){
+  for(let obj of imagesObjects){
+    obj.points = getRandomNumber(10);
+  }
+}
+
+let cards = document.querySelector(".cards");
 let usedQuestion = [];
 
-
-nextMeme()
+nextMeme();
 
 
 //делаем получатель карточек
@@ -215,7 +236,9 @@ function nextMeme() {
   console.log(usedQuestion); 
   if (usedQuestion.length == 10){
     console.log('finish');
-    return
+    main_page.style.display ="none"
+    result_page.style.display = "block";
+    return;
   }
 
   clean()
@@ -230,16 +253,16 @@ function clean(){
   cards.innerHTML = '';
 }
 
-function handleDragEnter() {
-  //console.log("dragenter");
-}
-
 function dragStart(event) {
   event.dataTransfer.setData("startId", event.target.id);
   //console.log("dragStart", event.target.id);
   let id = event.dataTransfer.getData("startId");
   //console.log(id);
   //console.log(event.target);
+}
+
+function handleDragEnter() {
+  //console.log("dragenter");
 }
 
 function handleDragLeave() {
